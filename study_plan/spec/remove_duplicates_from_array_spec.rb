fb468ec425b7b265f60e4d8ab704f2c13a7c02c3
Alex Aziz <alexhaziz@gmail.com>
2015-01-26 21:23:49 -0500

    Refactored specs
    - Converted methods to shared examples
    - Moved tests into context

--- study_plan/spec/remove_duplicates_from_array_spec.rb
+++ study_plan/spec/remove_duplicates_from_array_spec.rb
@@ -1,52 +1,63 @@
 require 'remove_duplicates_from_array'
 
 describe RemoveDuplicatesFromArray do
-
-  it "checks that no elments are removed from a sorted array with no duplicates" do
-    test_object = RemoveDuplicatesFromArray.new
-    array = test_object.array = [1,2,3]
-    array = test_object.remove_duplicates(array)
-    check_removal(array)
-  end
-
-  it "checks that a single duplicate element can be removed from a sorted array" do
-    test_object = RemoveDuplicatesFromArray.new
-    array = [1,2,2,3]
-    array = test_object.remove_duplicates(array)
-    check_removal(array)
-  end
-
-  it "checks that a single duplicate element can be removed from an unsorted array" do
-    test_object = RemoveDuplicatesFromArray.new
-    array = [4,5,0,20,1,2,2,3]
-    array = test_object.remove_duplicates(array)
-    check_removal(array)
-  end
-
-  it "checks that a several duplicate elements can be removed from a large unsorted array" do
-    test_object = RemoveDuplicatesFromArray.new
-    array = randomize_array(10000, 2)
-    array = test_object.remove_duplicates(array)
-    check_removal(array)
-  end
-
-  def check_removal(array)
-    array = array.sort
-    last_item = nil
-    array.each do |item|
-      if item == last_item
-        raise("Duplicate Found")
+  context "Verifies an array has no duplicates" do
+    shared_examples "check removal" do |array|
+      it "checks for duplicates" do
+        array = array.sort
+        last_item = nil
+        array.each do |item|
+          if item == last_item
+            raise("Duplicate Found")
+          end
+          last_item = item
+        end
       end
-      last_item = item
     end
-  end
-  
-  def randomize_array(size, random_range)
-    return_array = Array.new(size)
-    for i in 0..size
-      random_number = Random.new.rand(random_range)
-      return_array[i] = (random_number)
+
+    shared_examples "randomize array" do |size, random_range|
+      it "creates a random array and checks for duplicates" do
+        array = Array.new(size)
+        for i in 0..size
+          random_number = Random.new.rand(random_range)
+          array[i] = (random_number)
+        end
+        array = array.sort
+        test_object = RemoveDuplicatesFromArray.new
+        array = test_object.remove_duplicates(array)
+        last_item = nil
+        array.each do |item|
+          if item == last_item
+            raise("Duplicate Found")
+          end
+          last_item = item
+        end
+      end
     end
-    return_array
+
+    describe "sorted array with no duplicates" do
+      test_object = RemoveDuplicatesFromArray.new
+      array = test_object.array = [1,2,3]
+      array = test_object.remove_duplicates(array)
+      it_should_behave_like "check removal", array
+    end
+
+    describe "sorted array with single duplicate" do
+      test_object = RemoveDuplicatesFromArray.new
+      array = [1,2,2,3]
+      array = test_object.remove_duplicates(array)
+      it_should_behave_like "check removal", array
+    end
+
+    describe "unsorted array with single duplicate" do
+      test_object = RemoveDuplicatesFromArray.new
+      array = [4,5,0,20,1,2,2,3]
+      array = test_object.remove_duplicates(array)
+      it_should_behave_like "check removal", array
+    end
+
+    describe "large unsorted array with several duplicates" do
+      it_should_behave_like "randomize array", 10000, 2
+    end
   end
 end
