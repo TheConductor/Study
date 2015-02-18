db96f7d62ee2fd5bd7215a9891a0134b71d62aca
Alex Aziz <alexhaziz@gmail.com>
2015-02-18 11:49:00 -0500

    Completed some more basic c++ syntax exerices.

---
+++ study_plan/learning_c++/lib/intializing_vars.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    ///{ Write your code here
+    string name = "Neel";
+    int age = 18;
+   ///}
+   cout << name << " " << age << endl;
+     return 0;
+}
---
+++ study_plan/learning_c++/lib/variable_value.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+  {
+    char ch;
+    int x;
+    cout<<"Enter the character:\n";
+    cin>>ch;
+    ///{ Write your code here
+    x  = ch - 0;
+    ///}
+    cout<<x<<"\n";
+    return 0;
+  }
